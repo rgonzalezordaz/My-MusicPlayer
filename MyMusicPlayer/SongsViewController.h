ba8671641ab06a67839db700b9ce74ad23d7f4ee
Ricardo Gonzalez <Ricardo@macbook-pro-de-ricardo.local>
2016-08-21 00:53:47 -0500

    Second commit, Framework has been added.

---
+++ MyMusicPlayer/SongsViewController.h
@@ -0,0 +1,15 @@
+//
+//  SongsViewController.h
+//  MyMusicPlayer
+//
+//  Created by Ricardo Gonzalez on 21/08/16.
+//  Copyright © 2016 RGO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+
+@interface SongsViewController : UITableViewController
+
+@end
